912ce6de7f2da04101cc47b433060c503fa61644
Tomohito YABU <tyabu1212@gmail.com>
2019-05-13 22:44:48 +0900

    Add unique-paths-ii.

---
+++ unique-paths-ii/1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        long dp[101][101];
+        const int m = obstacleGrid.size();
+        const int n = obstacleGrid[0].size();
+        dp[0][0] = (obstacleGrid[0][0] == 1 ? 0 : 1);
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = (obstacleGrid[i][0] == 1 ? 0 : dp[i - 1][0]);
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = (obstacleGrid[0][i] == 1 ? 0 : dp[0][i - 1]);
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) { // empty space
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
