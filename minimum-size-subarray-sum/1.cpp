5a36abc175e08616307ebf54703878192d4aca2e
Tomohito YABU <tyabu1212@gmail.com>
2019-06-09 23:34:52 +0900

    Add 209. Minimum Size Subarray Sum.

---
+++ minimum-size-subarray-sum/1.cpp
@@ -0,0 +1,19 @@
+// time: O(n)
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int beginIdx = 0;
+        int sum = 0;
+        long minLen = LONG_MAX;
+        for (int i = 0; i < nums.size(); i++) {
+            int newSum = sum + nums[i];
+            if (newSum >= s) {
+                while (newSum - nums[beginIdx] >= s) newSum -= nums[beginIdx++];
+                minLen = min(minLen, (long)(i - beginIdx + 1));
+            }
+            sum = newSum;
+        }
+        return (minLen == LONG_MAX ? 0 : (int)minLen);
+    }
+};
